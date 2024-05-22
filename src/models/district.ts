d0d3c6f21339552a00bbaee51065c094acb4565b
Kilkenni <hpittman89@outlook.com>
2024-05-22 16:29:57 +0300

    TODO: still needs fixing

--- src/models/district.ts
+++ src/models/district.ts
@@ -6,32 +6,28 @@ const districtSchema = new Schema({
     type: String,
     default: "Feature",
   },
-  // properties: {
-  //   type: Schema.Types.Mixed,
-  //   // required: true,
-  // },
-  // geometry: {
-  //   type: new Schema({
-  //     type: {
-  //       type: String,
-  //       default: "Polygon",
-  //       // required: true,
-  //     },
-  //     coordinates: {
-  //       type: Array,
-  //       // required: true,
-  //     },
-  //   }),
-  //   // required: true,
-  // },
-  // title: new Schema({
-  //   ru: { type: String, index: true },
-  //   ua: { type: String, index: true },
-  // }),
-  // id: {
-  //   type: String,
-  //   // unique: true,
-  // },  
+  properties: {
+    type: Schema.Types.Mixed,
+    // required: true,
+  },
+  geometry: {
+    type: new Schema({
+      type: {
+        type: String,
+        // default: "Polygon",
+        // required: true,
+      },
+      coordinates: {
+        type: Array,
+        // required: true,
+      },
+    }),
+    // required: true,
+  },
+  id: {
+    type: String,
+    // unique: true,
+  },  
 });
 
 // const joiUserParamsSchema = Joi.object({
