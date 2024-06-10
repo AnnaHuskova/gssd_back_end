04204c688233854b16067478feaef29d1070cf4d
Kilkenni <hpittman89@outlook.com>
2024-06-10 15:35:49 +0300

    moved db_host components into separate .env vars

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -1,14 +1,18 @@
-// require("dotenv").config();
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const {
   PORT,
-  DB_HOST,
+  DB_PROTOCOL,
+  DB_GUEST_USERNAME,
+  DB_GUEST_SEC,
+  DB_HOST: DB_ADDRESS,
   // JWT_ACCESS_SECRET,
   // JWT_REFRESH_SECRET,
 } = process.env;
+
+const DB_HOST:string = `${DB_PROTOCOL}${DB_GUEST_USERNAME}:${DB_GUEST_SEC}${DB_ADDRESS}`;
 
 export {
   PORT,
