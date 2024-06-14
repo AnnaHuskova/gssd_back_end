92e8217cda10489032ee94cf36cb139684748969
Kilkenni <hpittman89@outlook.com>
2024-06-14 12:55:20 +0300

    adding cors headers to reponses

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 // const logger = require("morgan");
-// const cors = require("cors");
+import cors from "cors";
 import createError from "./src/helpers/errors.js";
 
 import routes from "./src/routes/api";
@@ -9,15 +9,21 @@ const app = express();
 
 const logFormat = app.get("env") === "development" ? "dev" : "short";
 
+const corsOptions: cors.CorsOptions = {
+  optionsSuccessStatus: 200,
+}
+
 // app.use(logger(logFormat));
-// app.use(cors());
+//app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
+app.options("*", cors(corsOptions));
+
 //List of valid REST api routes
 // app.use("/api/", swaggerRouter);
-app.use("/api/districts", routes.districtRoutes);
-app.use("/api/green-areas", routes.greenAreaRoutes);
+app.use("/api/districts", cors(corsOptions), routes.districtRoutes);
+app.use("/api/green-areas", cors(corsOptions), routes.greenAreaRoutes);
 // app.use("/api/users", usersRouter);
 
 //Route 404
